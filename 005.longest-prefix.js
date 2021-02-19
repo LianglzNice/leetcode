595c951571a800ddb2c1b58d3a1726c755249333
LianglzNice <572107412@qq.com>
2021-02-19 17:41:19 +0800

    查找最长公共前缀

--- 005.longest-prefix.js
+++ 005.longest-prefix.js
@@ -1,3 +1,10 @@
+/**
+ * 解法一
+ * 纵向扫描法
+ * 取第一个字符串为基础
+ * 逐渐累加长度，与后面的字符串进行同长度对比
+ * 如果遇到不同的，则返回上一次比较的字符串
+ */
 let strs = ["flower","flow","flight","floa","flright","flght"];
 let longestCommonPrefix = (strs) => {
     let res = "";
