c6c8f538253c5d0537761562a84dd571e55ce2fd
LianglzNice <572107412@qq.com>
2021-03-02 18:32:06 +0800

    移除元素

---
+++ 009.remove-element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+let nums = [0,1,2,2,3,0,4,2], val = 2
+let removeElement = (nums, val) => {
+    for(let i=0; i<nums.length-1; i++){
+        for(let j=i; ){
+
+        }
+    }
+};
