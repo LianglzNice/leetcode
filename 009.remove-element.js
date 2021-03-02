b15646d284dcbc9f6412e5d2d4cb03e7f767c48d
LianglzNice <572107412@qq.com>
2021-03-02 23:21:46 +0800

    移除元素

--- 009.remove-element.js
+++ 009.remove-element.js
@@ -2,12 +2,31 @@
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
+ * 解法一
+ * 双指针法
  */
-let nums = [0,1,2,2,3,0,4,2], val = 2
+let nums = [0,1,2,2,4,5], val = 2
 let removeElement = (nums, val) => {
-    for(let i=0; i<nums.length-1; i++){
-        for(let j=i; ){
+    //前指针
+    let i = 0;
+    //后指针
+    let j = nums.length;
 
+    while(i != j){
+        if(nums[i] === val){
+            if(nums[i] !== nums[j-1]){
+                nums[i] = nums[j-1];
+                //直接释放交换后的值
+                nums[j-1] = null;
+                i++;
+                j--;
+            }else{
+                j--;
+            }
+        }else{
+            i++;
         }
     }
+    return i
 };
+console.log(removeElement(nums, val));
