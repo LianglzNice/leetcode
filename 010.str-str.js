3a450c37c0cc3f28edf1d556692966500e4fef4d
LianglzNice <572107412@qq.com>
2021-03-04 18:33:37 +0800

    实现indexOf()

--- 010.str-str.js
+++ 010.str-str.js
@@ -3,6 +3,8 @@
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
+ * 解法一
+ * 利用 split()
  */
 let haystack = "hello", needle = "ll";
 let strStr = (haystack, needle) => {
@@ -11,4 +13,38 @@ let strStr = (haystack, needle) => {
     if(arr.length > 1)  return arr[0].length;
     return -1;
 };
-console.log(strStr(haystack, needle));
+
+
+/**
+ * 解法二
+ * 利用 substring()
+ */
+let strStr = (haystack, needle) => {
+    if(!needle) return 0;
+    let n = haystack.length;
+    let l = needle.length;
+    for(let i=0; i<n-l+1; i++){
+        if(haystack.substring(i, i+l) === needle){
+            return i;
+        }
+    }
+    return -1;
+}
+
+/**
+ * 解法三
+ * 
+ */
+let _strStr = (haystack, needle) => {
+    if(!needle) return 0;
+    let n = haystack.length;
+    let l = needle.length;
+    
+    let i=0;
+    while(i < n-l+1){
+        if(haystack.charAt(i) === ){
+
+        }
+    }
+}
+console.log(_strStr(haystack, needle));
