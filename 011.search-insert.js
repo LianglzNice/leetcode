172b912b16fc64d610efa6599c6cb6b586f99b15
LianglzNice <572107412@qq.com>
2021-03-08 18:32:07 +0800

    搜索插入位置

---
+++ 011.search-insert.js
@@ -0,0 +1,17 @@
+/**
+ * 搜索插入位置
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let nums = [1,3,5,6], target = 5;
+let searchInsert = (nums, target) => {
+    if(nums.length === 0) return 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] > target){
+
+        }
+    }
+};
+
+searchInsert(nums, target);
