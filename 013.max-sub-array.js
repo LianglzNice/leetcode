6caf1df6c80bd327f31092f86d650c470d472f2d
LianglzNice <572107412@qq.com>
2021-03-10 18:33:20 +0800

    最大子序和

---
+++ 013.max-sub-array.js
@@ -0,0 +1,19 @@
+/**
+ * 最大子序和
+ * @param {number[]} nums
+ * @return {number}
+ * 解法一
+ * 动态规划
+ */
+ let maxSubArray = nums => {
+    
+};
+
+
+/**
+ * 最大子序和
+ * @param {number[]} nums
+ * @return {number}
+ * 解法二
+ * 贪心算法
+ */
